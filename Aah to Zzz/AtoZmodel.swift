74a0be755c8ef2c985a84947473db4c935a988b7
David Fierstein <studio@davidiad.com>
2016-02-18 13:09:39 -0800

    Initial setup.
    Add AtoZTableViewController in a container view within AtoZViewController.

---
+++ Aah to Zzz/AtoZmodel.swift
@@ -0,0 +1,9 @@
+//
+//  AtoZmodel.swift
+//  Aah to Zzz
+//
+//  Created by David Fierstein on 2/18/16.
+//  Copyright © 2016 David Fierstein. All rights reserved.
+//
+
+import Foundation
---
+++ Aah to Zzz/WordListCell.swift
@@ -0,0 +1,24 @@
+//
+//  WordListCell.swift
+//  Aah to Zzz
+//
+//  Created by David Fierstein on 2/18/16.
+//  Copyright © 2016 David Fierstein. All rights reserved.
+//
+
+import UIKit
+
+class WordListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
