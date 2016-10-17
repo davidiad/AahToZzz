de0a00dcde45eb1aeb5f7018eba4543b05f0e5a2
David Fierstein <studio@davidiad.com>
2016-10-16 20:19:27 -0700

    Set up progress view
    Begin the set up of a game progress (e.g. statistics) view accessed from the progress bar button item. Create a ProgressViewController, with a table that holds some of the words saved through the Core Data data model.

---
+++ Aah to Zzz/ProgressViewController.swift
@@ -0,0 +1,75 @@
+//
+//  ProgressViewController.swift
+//  AahToZzz
+//
+//  Created by David Fierstein on 10/16/16.
+//  Copyright © 2016 David Fierstein. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ProgressViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, NSFetchedResultsControllerDelegate {
+    
+    // MARK: - NSFetchedResultsController
+    lazy var sharedContext = {
+        CoreDataStackManager.sharedInstance().managedObjectContext
+    }()
+    
+    lazy var fetchedResultsController: NSFetchedResultsController = {
+        
+        let fetchRequest = NSFetchRequest(entityName: "Word")
+        fetchRequest.predicate = NSPredicate(format: "inCurrentList == %@", true)
+        // Add Sort Descriptors
+        let sortDescriptor = NSSortDescriptor(key: "word", ascending: true)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        
+        let fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: self.sharedContext, sectionNameKeyPath: nil, cacheName: nil)
+        
+        fetchedResultsController.delegate = self
+        
+        return fetchedResultsController
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        do {
+            try fetchedResultsController.performFetch()
+        } catch {
+            let fetchError = error as NSError
+            print("\(fetchError), \(fetchError.localizedDescription)")
+        }
+        
+        //needed?
+        //game = model.game
+
+    }
+
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        if let _ = fetchedResultsController.sections {
+            return 1
+        }
+        
+        return 0
+    }
+
+    func tableView(tableView:UITableView, numberOfRowsInSection section:Int) -> Int
+    {
+        return 20
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        let cell:UITableViewCell=UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "mycell")
+        cell.textLabel!.text="row#\(indexPath.row)"
+        //cell.detailTextLabel!.text="subtitle#\(indexPath.row)"
+        
+        if let word = fetchedResultsController.objectAtIndexPath(indexPath) as? Word {
+            cell.detailTextLabel!.text = word.word
+        }
+        
+        return cell
+    }
+
+}
