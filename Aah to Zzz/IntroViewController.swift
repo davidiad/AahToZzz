58de6f0598fce19457c9dfb72514cb53dac55aec
David Fierstein <studio@davidiad.com>
2018-02-18 23:05:41 -0800

    Add UIVisualEffect blur to Intro view controller background

--- Aah to Zzz/IntroViewController.swift
+++ Aah to Zzz/IntroViewController.swift
@@ -10,15 +10,69 @@ import UIKit
 
 class IntroViewController: UIViewController {
 
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var bg: UIImageView!
+    
     @IBAction func play(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        bg.layoutIfNeeded()
+        bg.layer.cornerRadius = 30.0
+        bg.layer.borderWidth = 2.0
 
-        view.backgroundColor = UIColor.clear
+        bg.layer.borderColor = UIColor.black.cgColor
+        bg.layer.masksToBounds = true
+        
         view.isOpaque = false
+        
+        // blur effect
+        bg.backgroundColor = .blue
+        bg.isOpaque = false
+    
+        let blurEffect = UIBlurEffect(style: .light)
+        let blurView = UIVisualEffectView(effect: blurEffect)
+        blurView.layer.cornerRadius = 30.0
+        blurView.layer.borderWidth = 2.0
+        blurView.layer.borderColor = Colors.desat_yellow.cgColor
+        blurView.translatesAutoresizingMaskIntoConstraints = false
+        blurView.layer.shadowColor = UIColor.black.cgColor
+        blurView.layer.shadowOpacity = 1
+        blurView.layer.shadowOffset = CGSize.zero
+        blurView.layer.shadowRadius = 3
+        view.insertSubview(blurView, at: 0)
+        
+        NSLayoutConstraint.activate([
+            blurView.heightAnchor.constraint(equalTo: stackView.heightAnchor),
+            blurView.leadingAnchor.constraint(equalTo: stackView.leadingAnchor),
+            blurView.topAnchor.constraint(equalTo: stackView.topAnchor),
+            blurView.trailingAnchor.constraint(equalTo: stackView.trailingAnchor)
+            ])
+        
+        let vibrancyEffect = UIVibrancyEffect(blurEffect: blurEffect)
+        let vibrancyView = UIVisualEffectView(effect: vibrancyEffect)
+        vibrancyView.translatesAutoresizingMaskIntoConstraints = false
+        //vibrancyView.contentView.addSubview(view)
+        blurView.layer.cornerRadius = 30
+        blurView.layer.masksToBounds = true
+       // blurView.layer.maskedCorners
+        blurView.contentView.addSubview(vibrancyView)
+        
+        NSLayoutConstraint.activate([
+            vibrancyView.heightAnchor.constraint(equalTo: blurView.contentView.heightAnchor),
+            vibrancyView.leadingAnchor.constraint(equalTo: blurView.contentView.leadingAnchor),
+            vibrancyView.topAnchor.constraint(equalTo: blurView.contentView.topAnchor),
+            vibrancyView.trailingAnchor.constraint(equalTo: blurView.contentView.trailingAnchor)
+            ])
+
+        NSLayoutConstraint.activate([
+            view.centerXAnchor.constraint(equalTo: vibrancyView.contentView.centerXAnchor),
+            view.centerYAnchor.constraint(equalTo: vibrancyView.contentView.centerYAnchor),
+            ])
+
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,14 +81,5 @@ class IntroViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
